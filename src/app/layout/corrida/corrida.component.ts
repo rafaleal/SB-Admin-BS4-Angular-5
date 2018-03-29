ff7935f039957da07e9503d49d191a64ef71adbf
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-03-29 18:25:13 -0300

    Add breadcrumb and transition animation into corrida template

--- src/app/layout/corrida/corrida.component.ts
+++ src/app/layout/corrida/corrida.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-corrida',
   templateUrl: './corrida.component.html',
-  styleUrls: ['./corrida.component.scss']
+  styleUrls: ['./corrida.component.scss'],
+  animations: [routerTransition()]
 })
 export class CorridaComponent implements OnInit {
 
--- src/app/layout/corrida/corrida.module.ts
+++ src/app/layout/corrida/corrida.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CorridaRoutingModule } from './corrida-routing.module';
 import { CorridaComponent } from './corrida.component';
+import { PageHeaderModule } from '../../shared';
 
 @NgModule({
   imports: [
     CommonModule,
-    CorridaRoutingModule
+    CorridaRoutingModule,
+    PageHeaderModule
   ],
   declarations: [CorridaComponent]
 })
