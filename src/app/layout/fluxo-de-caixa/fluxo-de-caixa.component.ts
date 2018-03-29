f01474d13cb726846a9c7f910d112783e3943fdb
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-03-29 18:25:44 -0300

    Add breadcrumb and transition animation into fluxo-de-caixa template

--- src/app/layout/fluxo-de-caixa/fluxo-de-caixa.component.ts
+++ src/app/layout/fluxo-de-caixa/fluxo-de-caixa.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-fluxo-de-caixa',
   templateUrl: './fluxo-de-caixa.component.html',
-  styleUrls: ['./fluxo-de-caixa.component.scss']
+  styleUrls: ['./fluxo-de-caixa.component.scss'],
+  animations: [routerTransition()]
 })
 export class FluxoDeCaixaComponent implements OnInit {
 
--- src/app/layout/fluxo-de-caixa/fluxo-de-caixa.module.ts
+++ src/app/layout/fluxo-de-caixa/fluxo-de-caixa.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { FluxoDeCaixaRoutingModule } from './fluxo-de-caixa-routing.module';
 import { FluxoDeCaixaComponent } from './fluxo-de-caixa.component';
 
+import { PageHeaderModule } from './../../shared';
+
 @NgModule({
   imports: [
     CommonModule,
-    FluxoDeCaixaRoutingModule
+    FluxoDeCaixaRoutingModule,
+    PageHeaderModule
   ],
   declarations: [FluxoDeCaixaComponent]
 })
