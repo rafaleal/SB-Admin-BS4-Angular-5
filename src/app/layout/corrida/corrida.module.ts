0cae70ebb4654bce3338fe88bce262d7be71ecde
Rafael Leal <leal.rafa22@gmail.com>
2018-03-31 01:15:06 -0300

    Import primeng's TableModule and DropdownModule; also FormsModule

--- src/app/layout/corrida/corrida.module.ts
+++ src/app/layout/corrida/corrida.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CorridaRoutingModule } from './corrida-routing.module';
 import { CorridaComponent } from './corrida.component';
 import { PageHeaderModule } from '../../shared';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     CorridaRoutingModule,
-    PageHeaderModule
+    PageHeaderModule,
+    TableModule,
+    DropdownModule,
+    FormsModule
   ],
   declarations: [CorridaComponent]
 })
