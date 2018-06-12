68c283daf7596eca9140ece0bb8bb4aed1b52795
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-06-12 20:34:37 -0300

    Create awesome table with multiple rowspan

--- src/app/layout/corrida/list-corrida/list-corrida.component.ts
+++ src/app/layout/corrida/list-corrida/list-corrida.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CorridaService } from '../corrida.service';
 import { routerTransition } from '../../../router.animations';
+import * as _ from 'lodash';
+import { Itinerario } from '../../../domain/itinerario';
 
 @Component({
   selector: 'app-list-corrida',
@@ -12,6 +14,7 @@ import { routerTransition } from '../../../router.animations';
 export class ListCorridaComponent implements OnInit {
 
     corrida: any[];
+    corrida2: any[];
     rowGroupMetadata: any;
     sales: any[];
     pagamento: any[];
@@ -31,11 +34,30 @@ export class ListCorridaComponent implements OnInit {
           itinerario: {ponto: [{endereco: 'Praca Alfredo Andersen', responsavel: 'Adilson', tempoEspera: '30min'},
           {endereco: 'Banco do Brasil - Praca Escramulhao', responsavel: 'Jadir', tempoEspera: ''}]},
           cliente: 'Mueller', biker: 'Rodrigo', preco: '35'},
-          // {id: 2, data: '07/06/2018', horarios: {horaEntrada: '9:00', horaColeta: '9:15', horaEntrega: ''},
-          // itinerario: {ponto: [{endereco: 'Praca Alfredo Andersen', responsavel: 'Adilson', tempoEspera: '30min'},
-          // {endereco: 'Banco do Brasil - Praca Escramulhao', responsavel: 'Jadir', tempoEspera: ''}]},
-          // cliente: 'Mueller', biker: 'Rodrigo', preco: '35'}
+          {id: 2, data: '07/06/2018', horarios: {horaEntrada: '9:00', horaColeta: '9:15', horaEntrega: ''},
+          itinerario: {ponto: [{endereco: 'Praca Alfredo Andersen', responsavel: 'Adilson', tempoEspera: '30min'},
+          {endereco: 'Banco do Brasil - Praca Escramulhao', responsavel: 'Jadir', tempoEspera: ''}]},
+          cliente: 'Mueller', biker: 'Rodrigo', preco: '35'}
         ];
+
+        this.corrida2 = [];
+        this.corrida.forEach(cor => {
+            cor.itinerario.ponto.forEach(p => {
+                this.corrida2.push({
+                    id: cor.id,
+                    data: cor.data,
+                    horarios: cor.horarios,
+                    itinerario: {
+                        endereco: p.endereco,
+                        responsavel: p.responsavel,
+                        tempoEspera: p.tempoEspera,
+                    },
+                    cliente: cor.cliente,
+                    biker: cor.biker,
+                    preco: cor.preco,
+                });
+            });
+        });
 
         this.pagamento = [
             {name: 'Dinheiro', id: 1},
@@ -48,6 +70,8 @@ export class ListCorridaComponent implements OnInit {
             {name: 'Iniciado', id: 2},
             {name: 'Concluido', id: 3}
         ];
+
+        this.updateRowGroupMetaData();
     }
 
       onClickAdd(): void {
@@ -61,5 +85,30 @@ export class ListCorridaComponent implements OnInit {
       onClickDelete(): void {
           //
       }
+
+      onSort() {
+        this.updateRowGroupMetaData();
+      }
+
+      updateRowGroupMetaData() {
+        this.rowGroupMetadata = {};
+        if (this.corrida2) {
+            for (let i = 0; i < this.corrida2.length; i++) {
+                const rowData = this.corrida2[i];
+                const id = rowData.id;
+                if (i == 0) {
+                    this.rowGroupMetadata[id] = { index: 0, size: 1 };
+                } else {
+                    const previousRowData = this.corrida2[i - 1];
+                    const previousRowGroup = previousRowData.id;
+                    if (id === previousRowGroup) {
+                        this.rowGroupMetadata[id].size++;
+                    } else {
+                        this.rowGroupMetadata[id] = { index: i, size: 1 };
+                    }
+                }
+            }
+        }
+    }
 
 }
