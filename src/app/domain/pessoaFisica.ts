d4a3c4ea1450daa18896c56ccb443b3e41fc264b
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-06-12 20:34:57 -0300

    Rename PessoaFisica attribute

--- src/app/domain/pessoaFisica.ts
+++ src/app/domain/pessoaFisica.ts
@@ -3,6 +3,6 @@ import { Pessoa } from './pessoa';
 export class PessoaFisica implements Pessoa {
     telefone: string;
     email: string;
-    nomeCompleto: string;
+    nome: string;
     cpf: string;
 }
