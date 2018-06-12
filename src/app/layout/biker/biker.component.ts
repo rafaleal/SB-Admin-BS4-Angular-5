f1fb939915c3915bb5f9ad01bd0ae92bc1dee95b
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-06-12 20:30:21 -0300

    Set biker component

--- src/app/layout/biker/biker.component.ts
+++ src/app/layout/biker/biker.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { Biker } from '../../domain/biker';
+import { BikerService } from './biker.service';
 
 @Component({
   selector: 'app-biker',
@@ -7,11 +9,73 @@ import { routerTransition } from '../../router.animations';
   styleUrls: ['./biker.component.scss'],
   animations: [routerTransition()]
 })
+
 export class BikerComponent implements OnInit {
+    displayDialog: boolean;
 
-  constructor() { }
+        biker: Biker = {} as Biker;
 
-  ngOnInit() {
-  }
+        selectedBiker: Biker;
 
+        newBiker: boolean;
+
+        bikers: Biker[];
+
+        cols: any[];
+
+        constructor(private bikerService: BikerService) { }
+
+        ngOnInit() {
+            // this.bikerService.getBikers().then(bikers => this.bikers = bikers);
+
+            this.cols = [
+                { field: 'id', header: 'Id' },
+                { field: 'nome', header: 'Nome' },
+                { field: 'cpf', header: 'CPF' },
+                { field: 'telefone', header: 'Telefone' },
+                { field: 'email', header: 'Email' },
+                { field: 'numero', header: 'Numero de Corridas'},
+                { field: 'distancia', header: 'Distancia Total' }
+            ];
+        }
+
+        showDialogToAdd() {
+            this.newBiker = true;
+            this.biker = {} as Biker;
+            this.displayDialog = true;
+        }
+
+        save() {
+            const bikers = [...this.bikers];
+            if (this.newBiker) {
+                bikers.push(this.biker);
+            } else {
+                bikers[this.bikers.indexOf(this.selectedBiker)] = this.biker;
+            }
+
+            this.bikers = bikers;
+            this.biker = null;
+            this.displayDialog = false;
+        }
+
+        delete() {
+            const index = this.bikers.indexOf(this.selectedBiker);
+            this.bikers = this.bikers.filter((val, i) => i != index);
+            this.biker = null;
+            this.displayDialog = false;
+        }
+
+        onRowSelect(event) {
+            this.newBiker = false;
+            this.biker = this.cloneBiker(event.data);
+            this.displayDialog = true;
+        }
+
+        cloneBiker(b: Biker): Biker {
+            const biker = {} as Biker;
+            // for (let prop in b) {
+            //     biker[prop] = b[prop];
+            // }
+            return biker;
+        }
 }
--- src/app/layout/biker/biker.module.ts
+++ src/app/layout/biker/biker.module.ts
@@ -4,13 +4,25 @@ import { CommonModule } from '@angular/common';
 import { BikerRoutingModule } from './biker-routing.module';
 import { BikerComponent } from './biker.component';
 import { PageHeaderModule } from '../../shared';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { BikerService } from './biker.service';
 
 @NgModule({
   imports: [
     CommonModule,
     BikerRoutingModule,
-    PageHeaderModule
+    PageHeaderModule,
+    TableModule,
+    DialogModule,
+    FormsModule,
+    ButtonModule
   ],
-  declarations: [BikerComponent]
+  declarations: [BikerComponent],
+  providers: [
+      BikerService
+  ]
 })
 export class BikerModule { }
