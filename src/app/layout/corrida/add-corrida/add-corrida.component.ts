2e3a8e0c9c93bd6e0d83ec4206cd637b4c8067f5
Leal, Rafael <Rafael.Leal@mastercard.com>
2018-06-14 01:47:40 -0300

    initial steps add-corrida

--- src/app/layout/corrida/add-corrida/add-corrida.component.ts
+++ src/app/layout/corrida/add-corrida/add-corrida.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { Cliente } from '../../../domain/cliente';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Corrida } from '../../../domain/corrida';
+import { StatusEntrega } from '../../../domain/status-entrega';
+import { StatusEntregaEnum, StatusPagamentoEnum } from '../../../domain/enums';
 
 @Component({
   selector: 'app-add-corrida',
@@ -13,6 +16,7 @@ export class AddCorridaComponent implements OnInit {
 
   allClientes: Cliente[];
   selectedCliente: Cliente;
+  corrida: Corrida;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,8 +26,13 @@ export class AddCorridaComponent implements OnInit {
   ngOnInit() {
   }
 
-  loadCliente() {
+  loadClientes() {
     //
+  }
+
+  newCorridaEsporadico() {
+      this.corrida.statusEntrega = StatusEntregaEnum.REGISTRADO.toString();
+      this.corrida.pagamento = StatusPagamentoEnum.PENDENTE.toString();
   }
 
 }
