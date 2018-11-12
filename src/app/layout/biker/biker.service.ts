c39cbedb11c3c999278b26e901ae0ac4f0ab370f
Rafael Leal <leal.rafa22@gmail.com>
2018-11-12 00:19:22 -0200

    Create getBikersSummary and postBiker service methods

--- src/app/layout/biker/biker.service.ts
+++ src/app/layout/biker/biker.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
+import * as CONFIG from '../../constants';
+import { Biker } from '../../domain/biker';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+  })
+};
 
 @Injectable()
 export class BikerService {
 
-  constructor() { }
+  bikersSummaryURL: string = CONFIG.BIKER_SUMMARY_API_URL;
+  bikerPostURL: string = CONFIG.BIKER_POST_API_URL;
 
-  getBikers() {
-      //
+  constructor(
+    private http: HttpClient) {}
+
+  getBikersSummary(): Observable<Biker[]> {
+      return this.http.get<Biker[]>(this.bikersSummaryURL);
+  }
+
+  postBiker(biker: Biker): Observable<Biker> {
+      return this.http.post<Biker>(this.bikerPostURL, biker, httpOptions);
   }
 
 }
