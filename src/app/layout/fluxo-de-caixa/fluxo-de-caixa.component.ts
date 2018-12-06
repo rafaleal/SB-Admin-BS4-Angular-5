48968b68d1eeb8f10d9b7227fab45cb8c8aadc40
Rafael Leal <leal.rafa22@gmail.com>
2018-12-06 02:58:21 -0200

    Add pipe and create new table

--- src/app/layout/fluxo-de-caixa/fluxo-de-caixa.component.ts
+++ src/app/layout/fluxo-de-caixa/fluxo-de-caixa.component.ts
@@ -3,6 +3,7 @@ import { routerTransition } from '../../router.animations';
 import { CustomerCashFlow } from '../../domain/cash-flow/customer-cash-flow';
 import { FluxoDeCaixaService } from './fluxo-de-caixa.service';
 import { Biker } from '../../domain/biker';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-fluxo-de-caixa',
@@ -18,17 +19,30 @@ export class FluxoDeCaixaComponent implements OnInit {
     bikerCols: any[];
     bikersCashFlow: Biker[] = [];
 
+    expensesCols: any[];
+    expenses: any[] = [];
+
+    profit: number;
+
     constructor(private cashFlowService: FluxoDeCaixaService) { }
 
     ngOnInit() {
         this.populateCustomerCashFlowDetailsTable();
         this.populateBikersCashFlowDetailsTable();
+        this.populateExpensesDetailsTable();
 
         this.cashFlowService.getBikersCashFlow().subscribe(data =>  {
             this.bikersCashFlow = data;
             this.bikersCashFlow.forEach((value) => value.totalDue *= 0.7);
         });
         this.cashFlowService.getCustomersCashFlow().subscribe(data => this.customersCashFlow = data);
+        this.expenses = [
+            { nature: 'Aluguel', amount: 800.00 },
+            { nature: 'Telefone Fixo', amount: 30.00 },
+            { nature: 'Internet', amount: 130.00 },
+            { nature: 'Hospedagem Site', amount: 199.00}
+        ];
+        this.calculateProfit();
     }
 
     populateCustomerCashFlowDetailsTable(): void {
@@ -44,6 +58,20 @@ export class FluxoDeCaixaComponent implements OnInit {
             { field: 'fullName', header: 'Biker' },
             { field: 'totalDue', header: 'Salário' }
         ];
+    }
+
+    populateExpensesDetailsTable(): void {
+        this.expensesCols = [
+            { field: 'nature', header: 'Natureza' },
+            { field: 'amount', header: 'Valor' }
+        ];
+    }
+
+    calculateProfit() {
+        const expenses = _.sumBy(this.expenses, 'amount') + _.sumBy(this.bikersCashFlow, 'totalDue');
+        console.log(expenses);
+        const winnings = _.sumBy(this.customersCashFlow, 'totalAmount');
+        this.profit = winnings - expenses;
     }
 
 }
--- src/app/layout/fluxo-de-caixa/fluxo-de-caixa.module.ts
+++ src/app/layout/fluxo-de-caixa/fluxo-de-caixa.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FluxoDeCaixaRoutingModule } from './fluxo-de-caixa-routing.module';
 import { FluxoDeCaixaComponent } from './fluxo-de-caixa.component';
 
-import { PageHeaderModule } from './../../shared';
+import { PageHeaderModule, SharedPipesModule } from './../../shared';
 import { FluxoDeCaixaService } from './fluxo-de-caixa.service';
 import { BikerService } from '../biker/biker.service';
 import { BikerModule } from '../biker/biker.module';
@@ -14,6 +14,7 @@ import { BikerModule } from '../biker/biker.module';
     CommonModule,
     FluxoDeCaixaRoutingModule,
     PageHeaderModule,
+    SharedPipesModule
   ],
   declarations: [FluxoDeCaixaComponent],
   providers: [FluxoDeCaixaService]
